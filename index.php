d1edad10329e7a1a6e64a76af04d5a4188397243
vincenzodivita <divitavincenzo@icloud.com>
2023-06-23 01:09:35 +0200

    Add type on construct fn, add better indentation on fn, remove old commented code

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Movie {
     public $img;
 
     // Construct fn for OBJ declaration
-    public function  __construct($title,$year, $language, $genre, $img)
+    public function  __construct(string $title, int $year, string $language, string $genre, string $img)
     {
         $this->title = $title;
         $this->year = $year;
@@ -21,59 +21,23 @@ class Movie {
     // Get metadata fn
     public function getData()
     {
-        return "Titolo: " . $this->title . "; <br> Anno di uscita: " . $this->year . "; <br> Lingua originale: " . $this->language . "; <br> Genere: " . $this->genre . '; <br> <img alt="Poster non disponibile"> <br> <hr>';
+        return
+            "Titolo: " . $this->title . "; <br>
+            Anno di uscita: " . $this->year . "; <br>
+            Lingua originale: " . $this->language . "; <br>
+            Genere: " . $this->genre . '; <br>
+            <img alt="Poster non disponibile"> <br>
+            <hr>'
+        ;
     }
 }
 
-
-// Objects declarations
-
-// Manual declaration
-/*
-$matrix = new Movie;
-$matrix->title = "The Matrix";
-$matrix->year = 1999;
-$matrix->language = "ENG";
-$matrix->genre = "Fantasy";
-$matrix->img = "(da aggiungere)"; //TODO: Add img
-
-$matrix_reloaded = new Movie;
-$matrix_reloaded->title = "The Matrix Reloaded";
-$matrix_reloaded->year = 2003;
-$matrix_reloaded->language = "ENG";
-$matrix_reloaded->genre = "Fantasy";
-$matrix_reloaded->img = "(da aggiungere)"; //TODO: Add img
-
-$matrix_revolutions = new Movie;
-$matrix_revolutions->title = "The Matrix Revolutions";
-$matrix_revolutions->year = 2003;
-$matrix_revolutions->language = "ENG";
-$matrix_revolutions->genre = "Fantasy";
-$matrix_revolutions->img = "(da aggiungere)"; //TODO: Add img
-
-$matrix_resurretions = new Movie;
-$matrix_resurretions->title = "The Matrix Resurrections";
-$matrix_resurretions->year = 2022;
-$matrix_resurretions->language = "ENG";
-$matrix_resurretions->genre = "Fantasy";
-$matrix_resurretions->img = "(da aggiungere)"; //TODO: Add img
-*/
-
-// Declaration using construct fn
+// Objs eclaration using construct fn
 $matrix = new Movie("The Matrix", 1999, "ENG", "Fantasy", "(da aggiungere)");
 $matrix_reloaded = new Movie("The Matrix Reloaded", 2003, "ENG", "Fantasy", "(da aggiungere)");
 $matrix_revolutions = new Movie("The Matrix Revolutions", 2003, "ENG", "Fantasy", "(da aggiungere)");
 $matrix_resurretions = new Movie("The Matrix Resurrections", 2022, "ENG", "Fantasy", "(da aggiungere)");
 //TODO: Add imgs
-
-
-// Temporary var_dump print
-/*
-var_dump($matrix);
-var_dump($matrix_reloaded);
-var_dump($matrix_revolutions);
-var_dump($matrix_resurretions);
-*/
 
 // Temporary echo print
 echo $matrix->getData();
