9c7bce94d96beb73961eb736f6151b9caa8af57a
vincenzodivita <divitavincenzo@icloud.com>
2023-06-22 23:30:06 +0200

    Add first obj

--- index.php
+++ index.php
@@ -6,3 +6,12 @@ class Movie {
     public $genre;
     public $img;
 }
+
+$matrix = new Movie;
+$matrix->title = "The Matrix";
+$matrix->year = 1999;
+$matrix->language = "eng";
+$matrix->genre = "fantasy";
+$matrix->img = "(da aggiungere)"; //TODO: Add img
+
+var_dump($matrix);
