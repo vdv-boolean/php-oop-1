665c01427008e0dc0f44416be991fe1d17d4369d
vincenzodivita <divitavincenzo@icloud.com>
2023-06-24 17:05:59 +0200

    Add html basic structure

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 
+// Include other php files
 include __DIR__ . '/models/movie.php';
 include __DIR__ . '/models/genre.php';
 include __DIR__ . '/db.php';
 
-// Temporaty echo print
-foreach ($movies as $movie) {
-    echo $movie->getData();
- }
-// TODO: print correctly genres array
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        // Temporaty echo print
+        foreach ($movies as $movie) {
+            echo $movie->getData();
+        }
+        // TODO: print correctly genres array
+    ?>
+    </body>
+</html>
+
