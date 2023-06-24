efae64bf6bfd20af5584db7053d6700d89e55909
vincenzodivita <divitavincenzo@icloud.com>
2023-06-24 19:18:09 +0200

    Add genre array

--- db.php
+++ db.php
@@ -1,12 +1,22 @@
 <?php
 
 // Language array
-$language = [
-    "ITA",
-    "ENG",
-    "JAP",
-    "FRA",
-    "ESP",
+$arrLang = [
+    "ITA",  // 0
+    "ENG",  // 1
+    "JAP",  // 2
+    "FRA",  // 3
+    "ESP",  // 4
+];
+
+// Genres array
+$arrGenre = [
+    "Horror",   // 0
+    "Fantasy",  // 1
+    "Action",   // 2
+    "Drama",    // 3
+    "Thriller", // 4
+    "Sci-fi"    // 5
 ];
 
 // Movie array
@@ -14,50 +24,50 @@ $movies = [
     new Movie(
         "The Matrix",
         1999,
-        $language[1],
+        $arrLang[1],
         [
-            new Genre("Fantasy"),
-            new Genre("Action"),
+            new Genre($arrGenre[2]),
+            new Genre($arrGenre[5]),
         ],
         ""
     ),
     new Movie(
         "The Matrix Reloaded",
         2003,
-        "$language[1]",
+        $arrLang[1],
         [
-            new Genre("Fantasy"),
-            new Genre("Action"),
+            new Genre($arrGenre[2]),
+            new Genre($arrGenre[5]),
         ],
         ""
     ),
     new Movie(
         "The Matrix Reloaded",
         2003,
-        "$language[1]",
+        $arrLang[1],
         [
-            new Genre("Fantasy"),
-            new Genre("Action"),
+            new Genre($arrGenre[2]),
+            new Genre($arrGenre[5]),
         ],
         ""
     ),
     new Movie(
         "The Matrix Revolutions",
         2003,
-        "$language[1]",
+        $arrLang[1],
         [
-            new Genre("Fantasy"),
-            new Genre("Action"),
+            new Genre($arrGenre[2]),
+            new Genre($arrGenre[5]),
         ],
         ""
     ),
     new Movie(
         "The Matrix Resurrections",
         2022,
-        "$language[1]",
+        $arrLang[1],
         [
-            new Genre("Fantasy"),
-            new Genre("Action"),
+            new Genre($arrGenre[2]),
+            new Genre($arrGenre[5]),
         ],
         ""
     ),
