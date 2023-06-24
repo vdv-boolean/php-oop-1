12d149e59fd1175ded4bb1c03148df9d56d47256
vincenzodivita <divitavincenzo@icloud.com>
2023-06-24 17:02:33 +0200

    Cut genre class and array

--- index.php
+++ index.php
@@ -1,73 +1,8 @@
 <?php
 
 include __DIR__ . '/models/movie.php';
-
-// Genre Class
-class Genre {
-    public $name;
-
-    // Construct fn for OBJ declaration
-    public function  __construct(string $name)
-    {
-        $this->name = $name;
-    }
-
-}
-
-// Array declaration using construct fn
-$movies = [
-    new Movie(
-        "The Matrix",
-        1999,
-        "ENG",
-        [
-            new Genre("Fantasy"),
-            new Genre("Action"),
-        ],
-        ""
-    ),
-    new Movie(
-        "The Matrix Reloaded",
-        2003,
-        "ENG",
-        [
-            new Genre("Fantasy"),
-            new Genre("Action"),
-        ],
-        ""
-    ),
-    new Movie(
-        "The Matrix Reloaded",
-        2003,
-        "ENG",
-        [
-            new Genre("Fantasy"),
-            new Genre("Action"),
-        ],
-        ""
-    ),
-    new Movie(
-        "The Matrix Revolutions",
-        2003,
-        "ENG",
-        [
-            new Genre("Fantasy"),
-            new Genre("Action"),
-        ],
-        ""
-    ),
-    new Movie(
-        "The Matrix Resurrections",
-        2022,
-        "ENG",
-        [
-            new Genre("Fantasy"),
-            new Genre("Action"),
-        ],
-        ""
-    ),
-] ;
-// TODO: Add imgs
+include __DIR__ . '/models/genre.php';
+include __DIR__ . '/db.php';
 
 // Temporaty echo print
 foreach ($movies as $movie) {
