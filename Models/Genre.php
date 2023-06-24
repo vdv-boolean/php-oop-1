ccb6b7b5ec574c749a09609b32ea11a01b30212a
vincenzodivita <divitavincenzo@icloud.com>
2023-06-24 17:01:56 +0200

    Past Genre class from indec

---
+++ Models/Genre.php
@@ -0,0 +1,13 @@
+<?php
+
+// Class declaration
+class Genre {
+    public $name;
+
+    // Construct fn for OBJ declaration
+    public function  __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+}
